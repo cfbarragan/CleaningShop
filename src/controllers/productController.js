42184a7c4679ce79391e16822786617def54abd4
John Doe <JohnDoe@example.com>
2017-04-20 16:52:14 -0300

    Se modifican las querys para soportar las categorias

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -31,15 +31,10 @@ var productController = function(navPanel,configs) {
                                         $push: {
                                             products : product}
                                     },
-                                    //{new: true},   // return new doc if one is upserted
                                     {upsert: true}, // insert the document if it does not exist
                                     function(err, results) {
                                     res.redirect('/admin/adminPrecios');
                                 });
-
-                            // categories.insert(category, function(err, results) {
-                            // });
-
                         });
             });
     };
@@ -71,9 +66,10 @@ var productController = function(navPanel,configs) {
     var editProduct = function(req,res) {
             var id = new ObjectId(req.body.productId);
             var price =  req.body.productPrice;
-            var url = 'mongodb://cleanGenUser:jime150187@ds057816.mlab.com:57816/cleanshop';
+            var url = configs.DataBaseUrl;
             mongodb.connect(url, function(err,db) {
                 var collection = db.collection('products');
+                var categories = db.collection('category');
                 collection.updateOne(
                     {_id:id},
                     {
@@ -86,7 +82,14 @@ var productController = function(navPanel,configs) {
                         if (err != null) {
                             console.log(err);
                         }else {
-                            res.redirect('/admin/adminPrecios');
+                            categories.update(
+                                {'products._id': id},
+                                    {'$set': {
+                                        'products.$.productPrice' : price
+                                    }
+                            }, function(err, results) {
+                                    res.redirect('/admin/adminPrecios');
+                                });
                         }
                     });
             });
@@ -94,18 +97,23 @@ var productController = function(navPanel,configs) {
 
     var deleteProduct = function(req,res) {
             var id = new ObjectId(req.params.id);
-            var url = 'mongodb://cleanGenUser:jime150187@ds057816.mlab.com:57816/cleanshop';
+            var url = configs.DataBaseUrl;
             mongodb.connect(url, function(err,db) {
                 var collection = db.collection('products');
+                var categories = db.collection('category');
                 collection.remove({_id:id},
                     function(err, results) {
-                        if (results != null) {
-                            console.log(results);
-                            var product = {
-                                productName : results.productName,
-                                productPrice : results.productPrice
-                            };
+                        if (err != null) {
                             res.redirect('/admin/adminPrecios');
+                        }else {
+                            categories.update(
+                                {'products._id': id},
+                                    {'$pull': {
+                                        'products' :{'_id': id}
+                                    }
+                            }, function(err, results) {
+                                    res.redirect('/admin/adminPrecios');
+                                });
                         }
                     });
             });
